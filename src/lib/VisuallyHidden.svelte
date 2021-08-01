4dc8d9f2f7067ec4d7242eb55171ba4c0151dad3
yamanoku <0910yama@gmail.com>
2021-08-01 09:56:32 +0900

    init

---
+++ src/lib/VisuallyHidden.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let isFocusable = false;
+</script>
+
+<div class={isFocusable ? 'visually-hidden-focusable' : 'visually-hidden'}>
+  <slot />
+</div>
+
+<style>
+.visually-hidden,
+.visually-hidden-focusable:not(:focus):not(:focus-within) {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 4px;
+  height: 4px;
+  opacity: 0;
+  overflow: hidden;
+  border: none;
+  margin: 0;
+  padding: 0;
+  display: block;
+  visibility: visible;
+}
+</style>
